10685e92487a1b900af6abd27b9cc14c099ea4d1
Denys Vitali <denys@denv.it>
2023-07-16 01:51:57 +0200

    feat: add support for pictures and videos

--- src/components/SimplePost.vue
+++ src/components/SimplePost.vue
@@ -37,6 +37,60 @@ let c = computed(() => {
 const rootElement = ref(null);
 const rightElement = ref(null);
 
+
+const imageExpanded = ref(false);
+const videoExpanded = ref(false);
+
+const postVideo = computed(() => {
+    if (props.post.link == null) {
+        return '';
+    }
+
+    try {
+        let url = new URL(props.post.link);
+        let validHosts = ['i.imgur.com'];
+        if (!validHosts.includes(url.hostname)) {
+            return '';
+        }
+
+        // Valid extensions:
+        let validExtensions = ['mp4'];
+        let extension = url.pathname.split('.').pop() || '';
+        if (validExtensions.includes(extension)) {
+            return props.post.link;
+        }
+
+        return '';
+    } catch(e) {
+        return '';
+    }
+})
+
+const postImage = computed(() => {
+    if (props.post.link == null) {
+        return '';
+    }
+
+    try {
+        let url = new URL(props.post.link);
+        let validHosts = ['i.imgur.com'];
+        if (!validHosts.includes(url.hostname)) {
+            return '';
+        }
+
+        // Valid extensions:
+        let validExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+        let extension = url.pathname.split('.').pop() || '';
+        if (validExtensions.includes(extension)) {
+            return props.post.link;
+        }
+
+        return '';
+    } catch(e) {
+        return '';
+    }
+})
+
 const visitStory = (event: MouseEvent) => {
     if (event.target == rootElement.value || event.target == rightElement.value) {
         router.push(`/r/${props.post.ringName}/${props.post.id}`);
@@ -89,69 +143,115 @@ const voteDown = () => {
     })
 }
 
+const expandImage = () => {
+    if (postImage.value == null) {
+        return;
+    }
+    imageExpanded.value = !imageExpanded.value;
+}
+
+const expandVideo = () => {
+    if (postVideo.value == null) {
+        return;
+    }
+    videoExpanded.value = !videoExpanded.value;
+}
+
 </script>
 
 <template>
     <div class="post" @click="visitStory" ref="rootElement">
-        <div class="post-left">
-            <!-- Upvote / Downvote area, in a stylish way -->
-            <VoteContainer :score="post.score" 
-                :voted-up="post.votedUp" 
-                :voted-down="post.votedDown" 
-                @vote-up="voteUp"
-                @vote-down="voteDown" 
-            />
-        </div>
 
-        <div class="post-right" ref="rightElement">
-            <div v-if="post.link != null" class="post-title-container">
-                <a :href="post.link" class="post-link" target="_blank">
-                    <span class="post-title">{{ post.title }}</span>
-                </a>
+            <div class="post-left">
+                <!-- Upvote / Downvote area, in a stylish way -->
+                <VoteContainer :score="post.score" 
+                    :voted-up="post.votedUp" 
+                    :voted-down="post.votedDown" 
+                    @vote-up="voteUp"
+                    @vote-down="voteDown" 
+                />
             </div>
-            <div v-else class="post-title-container">
-                <RouterLink :to="`/r/${post.ringName}/${post.id}`" class="post-text">
-                    <span class="post-title">{{ post.title }}</span>
-                </RouterLink>
-            </div>
-            <div class="post-metadata">
-                <div class="domain element-divider" v-if="post.domain">
-                    <span class="post-domain">{{ post.domain }}</span>
-                    <span class="post-divider">•</span>
+
+            <div class="post-right" ref="rightElement">
+                <div v-if="post.link != null" class="post-title-container">
+                    <a :href="post.link" class="post-link" target="_blank">
+                        <span class="post-title">{{ post.title }}</span>
+                    </a>
                 </div>
-                <div class="ring element-divider" v-if="props.singlePostView || multiRing">
-                    <RingLink :name="post.ringName" :color="post.ringColor"></RingLink>
-                    <span class="post-divider">•</span>
+                <div v-else class="post-title-container">
+                    <RouterLink :to="`/r/${post.ringName}/${post.id}`" class="post-text">
+                        <span class="post-title">{{ post.title }}</span>
+                    </RouterLink>
                 </div>
-                <div class="author element-divider">
-                    <div class="author-content">
-                        <ProfilePicture :username="post.authorUsername" class="post-author-profile-picture" />
-                        <UserLink :username="post.authorUsername" :admin="post.author.admin" class="post-author-username" />
+                <div class="post-metadata">
+                    <div class="domain element-divider" v-if="post.domain">
+                        <span class="post-domain">{{ post.domain }}</span>
+                        <span class="post-divider">•</span>
                     </div>
-                    <span class="post-divider">•</span>
+                    <div class="ring element-divider" v-if="props.singlePostView || multiRing">
+                        <RingLink :name="post.ringName" :color="post.ringColor"></RingLink>
+                        <span class="post-divider">•</span>
+                    </div>
+                    <div class="author element-divider">
+                        <div class="author-content">
+                            <ProfilePicture :username="post.authorUsername" class="post-author-profile-picture" />
+                            <UserLink :username="post.authorUsername" :admin="post.author.admin" class="post-author-username" />
+                        </div>
+                        <span class="post-divider">•</span>
+                    </div>
+                    <div class="date element-divider">
+                        <span class="post-date" :alt="c.postedOnYmd">{{ c.postedOn }}</span>
+                    </div>
+                    <span class="nsfw-tag" v-if="post.nsfw">NSFW</span>
+                    <div class="post-actions element-divider" v-if="!singlePostView">
+                        <span class="post-divider">•</span>
+                        <span class="post-action">
+                            <RouterLink :to="`/r/${post.ringName}/${post.id}`">
+                                <FontAwesomeIcon class="icon" :icon="['fas', 'message']" />
+                                <span class="post-action-text">{{ post.commentsCount }}</span>
+                            </RouterLink>
+                        </span>
+                    </div>
                 </div>
-                <div class="date element-divider">
-                    <span class="post-date" :alt="c.postedOnYmd">{{ c.postedOn }}</span>
+
+                <div class="post-body" v-if="post.body != null" :class="{ 'faded': !singlePostView }">
+                    <div class="faded-overlay" v-if="!singlePostView"></div>
+                    <RenderedMarkdown :markdown="post.body" />
                 </div>
-                <span class="nsfw-tag" v-if="post.nsfw">NSFW</span>
-                <div class="post-actions element-divider" v-if="!singlePostView">
-                    <span class="post-divider">•</span>
-                    <span class="post-action">
-                        <RouterLink :to="`/r/${post.ringName}/${post.id}`">
-                            <FontAwesomeIcon class="icon" :icon="['fas', 'message']" />
-                            <span class="post-action-text">{{ post.commentsCount }}</span>
-                        </RouterLink>
-                    </span>
-                </div>
+
+                <img 
+                    :src="postImage"
+                    @click="expandImage"
+                    class="post-image"
+                    :class="{ 'post-image': true, 'expanded': imageExpanded }"
+                    v-if="post.link != null"
+                    alt="Post Image">
+
+                <video
+                    class="post-video"
+                    controls
+                    :class="{ 'expanded': videoExpanded }"
+                    v-if="postVideo != ''"    
+                >
+                    <source :src="postVideo" type="video/mp4">
+                </video>
             </div>
 
-            <div class="post-body" v-if="post.body != null" :class="{ 'faded': !singlePostView }">
-                <div class="faded-overlay" v-if="!singlePostView"></div>
-                <RenderedMarkdown :markdown="post.body" />
-            </div>
-        </div>
-
-        <img :src="`https://picsum.photos/seed/${post.id}/200/300`" class="post-image" alt="Post Image">
+        <img 
+            :src="postImage"
+            @click="expandImage"
+            class="post-image-inline"
+            :class="{ 'expanded': imageExpanded }"
+            v-if="postImage != ''"
+            alt="Post Image">
+        <video
+            class="post-video-inline"
+            @click="expandVideo"
+            :class="{ 'expanded': videoExpanded }"
+            v-if="postVideo != ''"    
+        >
+            <source :src="postVideo" type="video/mp4">
+        </video>
     </div>
 </template>
 
@@ -162,28 +262,43 @@ $authorLineHeight: 24px;
     display: flex;
     flex-direction: row;
     width: 100%;
-    column-gap: var(--generic-column-gap);
-
     background-color: var(--color-post-background);
     border: var(--generic-border);
     padding: var(--generic-padding);
+    column-gap: var(--generic-column-gap);
     border-radius: var(--generic-border-radius);
-    overflow: hidden;
-    max-height: 250px;
-
-    align-items: center;
+    row-gap: 14px;
 
 
-    .post-image {
+
+    .post-image-inline, .post-video-inline {
+        cursor: pointer;
         height: 80px;
         width: 80px;
         flex-shrink: 1;
         object-fit: cover;
+
+        &.expanded {
+            opacity: 0;
+        }
     }
 
     .post-right {
         flex: 1;
         justify-content: flex-start;
+
+        .post-image, .post-video {
+            display: none;
+            max-height: 500px;
+            margin-top: 14px;
+            margin-bottom: 14px;
+            align-self: flex-start;
+            object-fit: contain;
+
+            &.expanded {
+                display: block;
+            }
+        }
     }
 
     .post-left {
